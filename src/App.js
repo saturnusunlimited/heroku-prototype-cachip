1b12309c8a713474ff7971f84feeb4fbb915c2f3
Aad Schippers <saturnusunlimited@gmail.com>
2019-10-20 15:07:37 +0200

    Added bootstrap and refresh button in boostrap style

--- src/App.js
+++ src/App.js
@@ -1,90 +1,108 @@
 import React from 'react';
 import double_quotes from './assets/svg/double_quotes.svg';
 import './App.css';
+import './css/CustomBootstrap.css'
 import quotes from './assets/json/quotes.json'
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-var quote = randomQuote();
-
-function randomQuote() {
-
-    var index = Math.floor(Math.random() * quotes.length);
-    var quote = quotes[index];
-
-    if (quote.quoteAuthor === "") {
-
-        quote.quoteAuthor = "Anonymous";
-    }
-
-    return quote;
-}
 
 function App() {
 
-  return (
+    let [quote, setQuote] = useState(randomQuote());
 
-    <div className="App">
+    function randomQuote() {
 
-      <header className="App-header">
+        var index = Math.floor(Math.random() * quotes.length);
+        var quote = quotes[index];
 
-        <h1>Quote</h1>
+        if (quote.quoteAuthor === "") {
 
-        <div className="App-quote">
+            quote.quoteAuthor = "Anonymous";
+        }
 
-            <div className="quotes">
-                <img src={double_quotes} className="App-quotes-left" alt="double quotes"/>
+        return quote;
+    }
+
+    function newRandomQuote () {
+
+        setQuote(randomQuote());
+    }
+
+    function toggleLeetSpeak () {
+
+        // <Button bsStyle="secondary" onClick={toggleLeetSpeak}>Leet Speak</Button>
+        alert('toggle leet speak');
+    }
+
+    // DEVELOPER NOTE: The return App template should probably be in a separate
+    // file. Investigate ReactJS best practices for project structure.
+    //
+    return (
+
+        <div className="App">
+
+          <header className="App-header">
+
+            <Button bsStyle="primary" onClick={newRandomQuote}>Change Quote</Button>
+
+            <div className="App-quote">
+
+                <div className="quotes">
+                    <img src={double_quotes} className="App-quotes-left" alt="double quotes"/>
+                </div>
+
+                <div>
+                    <p className="App-quote-text">{quote.quoteText}</p>
+                </div>
+
+                <div className="quotes" >
+                    <img src={double_quotes} className="App-quotes-right" alt="double quotes"/>
+                </div>
             </div>
 
-            <div>
-                <p className="App-quote-text">{quote.quoteText}</p>
+            <div className="App-quote">
+
+                <p className="App-quote-author">{quote.quoteAuthor}</p>
             </div>
 
-            <div className="quotes" >
-                <img src={double_quotes} className="App-quotes-right" alt="double quotes"/>
+            <div className="App-credits">
+
+                <p>
+                    Icons made by <a
+                    className="App-link"
+                    href="https://www.flaticon.com/authors/freepik"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Freepik">Freepik</a> from <a
+                    className="App-link"
+                    href="https://www.flaticon.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Flaticon">www.flaticon.com
+                    </a>
+                </p>
+
+                <p>
+                    This app was made with <a
+                      className="App-link"
+                      href="https://create-react-app.dev/docs/getting-started#!"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    > create-react-app </a>
+                </p>
+
+                <p>
+                   Comments or problems? Mail <a className="App-link" href="mailto:saturnusunlimited@gmail.com"> saturnusunlimited@gmail.com </a>
+                </p>
+
             </div>
-        </div>
 
-        <div className="App-quote">
 
-            <p className="App-quote-author">{quote.quoteAuthor}</p>
-        </div>
-
-        <div className="App-credits">
-
-            <p>
-                Icons made by <a
-                className="App-link"
-                href="https://www.flaticon.com/authors/freepik"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Freepik">Freepik</a> from <a
-                className="App-link"
-                href="https://www.flaticon.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Flaticon">www.flaticon.com
-                </a>
-            </p>
-
-            <p>
-                This app was made with <a
-                  className="App-link"
-                  href="https://create-react-app.dev/docs/getting-started#!"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                > create-react-app </a>
-            </p>
-
-            <p>
-               Comments or problems? Mail <a className="App-link" href="mailto:saturnusunlimited@gmail.com"> saturnusunlimited@gmail.com </a>
-            </p>
+          </header>
 
         </div>
-
-
-      </header>
-
-    </div>
-  );
+    );
 }
 
 export default App;
