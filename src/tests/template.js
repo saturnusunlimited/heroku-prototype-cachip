fb816e3a4bf380eb73b552f8aae56737e1c27453
Aad Schippers <saturnusunlimited@gmail.com>
2019-10-27 14:52:32 +0100

    Added test directory with jest test template

---
+++ src/tests/template.js
@@ -0,0 +1,18 @@
+// This is not really a test, but a template to generate tests from
+//
+function filterByTerm(inputArr, searchTerm) {
+  return inputArr.filter(function(arrayElement) {
+    return arrayElement.url.match(searchTerm);
+  });
+}
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+    expect(filterByTerm(input, "link")).toEqual(output);
+  });
+});
